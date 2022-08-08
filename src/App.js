0d6b8da179be9f1dbdd282629b54551ce0a86bf9
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2022-08-08 10:35:47 -0600

    crreated search functionality

--- src/App.js
+++ src/App.js
@@ -8,28 +8,53 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Zack',
-        },
-        {
-          name: 'Celeste',
-        },
-        {
-          name: 'Clare',
-        },
-        {
-          name: 'Kat',
-        },
-      ],
+      monsters: [],
+      searchField: '',
     };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    console.log('render');
+
+    const {monsters, searchField } = this.state
+    const { searchChange } = this;
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search Monsters'
+          onChange={ onSearchChange }
+        />
+
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
